70f80cdb2b21bc5a2af220e44b2b91045b553276
Shao-Lun Huang <simon.linux7@gmail.com>
2024-03-23 00:19:13 +0100

    Create a builder for TypeResult to generate different messages based on the type of the given inequality.

--- src/qitip/objects/type_result.py
+++ src/qitip/objects/type_result.py
@@ -1,9 +1,27 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from numpy.typing import NDArray
 
 
-@dataclass(kw_only=True)
+class ResultBuilder:
+    def __init__(self) -> None:
+        self.message: str = ""
+        self.status: Optional[bool] = None
+
+    def check_type(self, status: bool):
+        self.status = status
+
+        # Abbreviation for von-Neumann type
+        vn_message: str = "It's von-Neumann type inequality.\nIt can be proved by:\n"
+        # Abbreviation for non-Provable type
+        np_message: str = (
+            "Not provable by Quantum ITIP:(\nOne can try to disprove by using:\n"
+        )
+        self.message += vn_message if status else np_message
+
+
+@dataclass
 class TypeResult:
     status: bool
     inequality_entry: tuple[NDArray, ...] | tuple[()] | None
